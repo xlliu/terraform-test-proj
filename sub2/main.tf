a5b9d5800e25f3410faf371b92a347e1b4f62115
Maciej Skierkowski <maciej@skierkowski.com>
2018-01-16 11:27:29 -0800

    Update main.tf

--- sub2/main.tf
+++ sub2/main.tf
@@ -1,5 +1,11 @@
 variable "my_var" { }
 
+resource "null_resource" "my_null_resource" {
+  provisioner "local-exec" {
+    command = "echo hello"
+  }
+}
+
 output "my_output" {
   value = "echo, ${var.my_var}"
 }
