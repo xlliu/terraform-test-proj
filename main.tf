f4807b1877430a2d26496113b9ed5c94f3efd90a
Maciej Skierkowski <maciej@skierkowski.com>
2018-05-25 09:38:58 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "random_id" "random" {
   keepers {
     uuid = "${uuid()}"
   }
-  byte_length = 24
+  byte_length = 12
 }
 
 output "random" {
@@ -16,3 +16,4 @@ output "random" {
 output "hello_world" {
   value = "Hello, ${var.name}"
 }
+
